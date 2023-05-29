4beea5fffc0f8b6302b68baa5626ab67ab509869
s02singh <103849999+s02singh@users.noreply.github.com>
2023-05-29 03:43:04 -0700

    Delete SocialMediaCaptioner/image_caption_generator directory

--- SocialMediaCaptioner/image_caption_generator/caption_generator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CaptionGeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'caption_generator'
--- SocialMediaCaptioner/image_caption_generator/caption_generator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'caption_generator'
-
-urlpatterns = [
-    path('', views.generate_caption, name='generate_caption'),
-    
-]
